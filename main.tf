231e80580a19b1e81b8ae063dfc25f09537887fe
xav <xav@xav.xav>
2022-09-20 21:36:25 +0200

    add eks in main

--- main.tf
+++ main.tf
@@ -79,3 +79,18 @@ resource "aws_security_group" "k8s-sg" {
     }
 }
 
+resource "aws_eks_cluster" "example" {
+  name     = "mycluster"
+  role_arn = aws_iam_role.example.arn
+
+  vpc_config {
+    subnet_ids = [aws_subnet.example1.id, aws_subnet.example2.id]
+  }
+
+depends_on = [
+    aws_iam_role_policy_attachment.example-AmazonEKSClusterPolicy,
+    aws_iam_role_policy_attachment.example-AmazonEKSVPCResourceController,
+  ]
+}
+
+
