a6317f29dc3f1e40f46e4d9018cdd17b50806ccd
xav <xav@xav.xav>
2022-09-20 21:25:15 +0200

    add var secret

--- main.tf
+++ main.tf
@@ -9,8 +9,8 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
-  access_key = "************"
-  secret_key = "*******"
+  access_key = var.access_key 
+  secret_key = var.secret_key
   region = var.AWS_REGION
 }
 
